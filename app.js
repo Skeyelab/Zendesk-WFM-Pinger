0cfae624fb38c463422588c0d62805d5dc9f17e7
Eric Dahl <edahl@groupon.com>
2015-05-07 17:19:19 -0500

    cleanup

--- app.js
+++ app.js
@@ -1,43 +1,43 @@
 (function() {
 
-	return {
+  return {
 
-		requests: {
-			pingServer: function(data) {
-				return {
-					url: this.setting('wfm_server'),
-					type: 'POST',
-					data: data
-				};
-			}
-		},
-		events: {
-			'app.activated': function() {
-				this.ping("start");
-			},
-			'app.deactivated': function() {
-				this.ping("stop");
-			},
-			'app.willDestroy': function() {
-				this.ping("destroy");
-			},
-			'ticket.save': function() {
-				this.ping("save " + this.comment().type());
-			}
+    requests: {
+      pingServer: function(data) {
+        return {
+          url: this.setting('wfm_server'),
+          type: 'POST',
+          data: data
+        };
+      }
+    },
+    events: {
+      'app.activated': function() {
+        this.ping("start");
+      },
+      'app.deactivated': function() {
+        this.ping("stop");
+      },
+      'app.willDestroy': function() {
+        this.ping("destroy");
+      },
+      'ticket.save': function() {
+        this.ping("save " + this.comment().type());
+      }
 
-		},
-		ping: function(state) {
-			var data = {
-				"domain": this.currentAccount().subdomain(),
-				"ticket_id": this.ticket().id(),
-				"assignee_id": this.currentUser().id(),
-				"state": state,
-				"email": this.currentUser().email()
-			};
+    },
+    ping: function(state) {
+      var data = {
+        "domain": this.currentAccount().subdomain(),
+        "ticket_id": this.ticket().id(),
+        "assignee_id": this.currentUser().id(),
+        "state": state,
+        "email": this.currentUser().email()
+      };
 
-			this.ajax('pingServer', data);
-		},
+      this.ajax('pingServer', data);
+    },
 
-	};
+  };
 
 }());
